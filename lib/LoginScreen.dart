1c3d06a9ee60f81a3508287d870144f8235fd55a
YuunOoO <dariusz.lopian@gmail.com>
2022-04-16 16:04:20 +0200

    Login page ver 0.3

--- lib/LoginScreen.dart
+++ lib/LoginScreen.dart
@@ -6,6 +6,85 @@ class LoginScreen extends StatefulWidget {
   _LoginScreenState createState() => _LoginScreenState();
 }
 
+Widget buildEmail() {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        'Email',
+        style: TextStyle(
+            fontSize: 20, color: Colors.black, fontWeight: FontWeight.bold),
+      ),
+      SizedBox(height: 15),
+      Container(
+        alignment: Alignment.centerLeft,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black26, blurRadius: 6, offset: Offset(0, 2))
+            ]),
+        height: 60,
+        child: TextField(
+          keyboardType: TextInputType.emailAddress,
+          style: TextStyle(color: Colors.black87),
+          decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 16),
+              prefixIcon: Icon(
+                Icons.email,
+                color: Color(0xff000000),
+                size: 30,
+              ),
+              hintText: 'Email',
+              hintStyle: TextStyle(color: Colors.black38)),
+        ),
+      )
+    ],
+  );
+}
+
+Widget buildPassword() {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Padding(padding: EdgeInsets.only(top: 20)),
+      Text(
+        'Password',
+        style: TextStyle(
+            fontSize: 20, color: Colors.black, fontWeight: FontWeight.bold),
+      ),
+      SizedBox(height: 15),
+      Container(
+        alignment: Alignment.centerLeft,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black26, blurRadius: 6, offset: Offset(0, 2))
+            ]),
+        height: 60,
+        child: TextField(
+          keyboardType: TextInputType.emailAddress,
+          style: TextStyle(color: Colors.black87),
+          decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 16),
+              prefixIcon: Icon(
+                Icons.lock,
+                color: Color(0xff000000),
+                size: 30,
+              ),
+              hintText: 'Password',
+              hintStyle: TextStyle(color: Colors.black38)),
+        ),
+      )
+    ],
+  );
+}
+
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
@@ -16,6 +95,7 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Stack(
           children: <Widget>[
             Container(
+              padding: new EdgeInsets.all(10.0),
               height: double.infinity,
               width: double.infinity,
               decoration: BoxDecoration(
@@ -23,21 +103,24 @@ class _LoginScreenState extends State<LoginScreen> {
                       begin: Alignment.topCenter,
                       end: Alignment.bottomCenter,
                       colors: [
-                    Color(0xffc653c6),
-                    Color(0xcc993399),
-                    Color(0xB3732673),
-                    Color(0xff260d26),
+                    Color(0xcc606060),
+                    Color(0xcc0000FF),
+                    Color(0xD90000CC),
+                    Color(0xff660066),
                   ])),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   Text(
-                    'Zaloguj ',
+                    'Sign In',
                     style: TextStyle(
-                        color: Colors.grey,
+                        color: Colors.white,
                         fontSize: 70,
                         fontWeight: FontWeight.bold),
-                  )
+                  ),
+                  SizedBox(height: 50),
+                  buildEmail(),
+                  buildPassword()
                 ],
               ),
             )
