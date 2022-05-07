313ff884c562f06448560826c3836d2062ffa8b1
Jaqqas <kubak1332@gmail.com>
2022-05-07 21:21:01 +0200

    navbar

--- lib/Pages/geopage.dart
+++ lib/Pages/geopage.dart
@@ -1,6 +1,9 @@
 import 'package:dragcon/location.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import '../NavBar.dart';
+import '../main.dart';
+import 'dart:collection';
 
 class geopage extends StatefulWidget {
   const geopage({Key? key}) : super(key: key);
@@ -50,6 +53,7 @@ class _geopage extends State<geopage> {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
+      drawer: NavBar(),
       body: GoogleMap(
         markers: {for (int i = 0; i < _marker.length; i++) _marker[i]},
         mapType: MapType.normal,
