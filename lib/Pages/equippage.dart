1e239261025899100d9f08d288081b084abcc67e
Jaqqas <kubak1332@gmail.com>
2022-05-20 16:00:08 +0200

    Narzędzia scrollowanie

--- lib/Pages/equippage.dart
+++ lib/Pages/equippage.dart
@@ -35,55 +35,56 @@ class _equippage extends State<equippage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        drawer: NavBar(),
-        body: SingleChildScrollView(
-          child: Container(
-            height: 100.h,
-            width: 100.w,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("assets/images/japback.jpg"),
-                fit: BoxFit.cover,
-              ),
-            ),
-            child: list(context),
+      drawer: NavBar(),
+      body: Container(
+        height: 100.h,
+        width: 100.w,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/japback.jpg"),
+            fit: BoxFit.cover,
           ),
-        ));
+        ),
+        child: list(context),
+      ),
+    );
   }
 }
 
 Widget list(BuildContext context) {
-  return Column(children: [
-    ListView.builder(
-      shrinkWrap: true,
-      scrollDirection: Axis.vertical,
-      itemCount: leng,
-      itemBuilder: (context, index) {
-        return Container(
-            padding: EdgeInsets.all(10),
-            margin: EdgeInsets.all(10),
-            color: Color.fromARGB(255, 183, 131, 252),
-            child: ExpandablePanel(
-              theme: ExpandableThemeData(
-                  hasIcon: false,
-                  animationDuration: const Duration(milliseconds: 500)),
-              header: Text(
-                _tools[index].type,
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-              ),
-              expanded: Text(
-                "Marka narzędzia: " +
-                    _tools[index].mark +
-                    "\n Ilość: " +
-                    _tools[index].amount.toString(),
-                softWrap: true,
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-              ),
-              collapsed: Text(''),
-            ));
-      },
-    ),
-  ]);
+  return ListView.builder(
+    shrinkWrap: true,
+    scrollDirection: Axis.vertical,
+    itemCount: leng,
+    itemBuilder: (context, index) {
+      return ExpandableNotifier(
+          child: ScrollOnExpand(
+              child: Container(
+                  color: Color.fromARGB(123, 123, 123, 1),
+                  margin: EdgeInsets.all(10),
+                  padding: EdgeInsets.all(15),
+                  child: ExpandablePanel(
+                    theme: ExpandableThemeData(
+                        hasIcon: false,
+                        animationDuration: const Duration(milliseconds: 500)),
+                    header: Text(
+                      _tools[index].type,
+                      textAlign: TextAlign.center,
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
+                    expanded: Text(
+                      "Marka narzędzia: " +
+                          _tools[index].mark +
+                          "\n Ilość: " +
+                          _tools[index].amount.toString(),
+                      softWrap: true,
+                      textAlign: TextAlign.center,
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
+                    collapsed: Text(''),
+                  ))));
+    },
+  );
 }
